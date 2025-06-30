0c49fd2ec29e9b8ba9a3f112b3c41d608f57cdca
nym <roosternam@gmail.com>
2025-06-30 20:07:51 +0900

    build single agent

---
+++ langgraphs/lang_graph/bi_single_agent.py
@@ -0,0 +1,134 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+
+from dotenv import load_dotenv
+
+# open ai key랑 tavily api key 필요
+load_dotenv()
+
+# State 설정
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import add_messages
+from langchain_core.messages import SystemMessage, HumanMessage
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+# 도구 설정(웹 검색, PythonREPL)
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import ToolNode, tools_condition
+# 코드가 주어졌을 때 로컬환경에서 실행하도록 도와줌
+from langchain_experimental.utilities import PythonREPL
+from langchain_core.tools import tool
+
+web_search = TavilySearchResults(max_results=2)
+repl = PythonREPL()
+
+@tool
+def python_repl(
+    code: Annotated[str, "The python code to execute to generate your chart."]
+):
+    """Use this to execute python code. If you want to see the output of a value,
+    you should print it out with `print(...)`. chart labels should be written in English.
+    This is visible to the user."""
+    try:
+        result = repl.run(code)
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    result_str = f"Succesfully executed:\n```python\n{code}\n```\nStdout: {result}"
+    return (
+        result_str + "\n\nIf you have completed all tasks, respond with FINAL ANSWER."
+    )
+tools = [web_search, python_repl]
+tool_node = ToolNode(tools)
+
+# 에이전트에게 도구 인지시키기
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(model="gtp-4o-mini")
+llm_with_tools = llm.bind_tools(tools)
+
+def agent(state: State):
+    result = llm_with_tools.invoke(state["messages"])
+    return {"messages": [result]}
+
+def shold_continue(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+    
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import tools_condition
+from langgraph.graph import StateGraph
+from langgraph.checkpoint.memory import MemorySaver
+
+workflow = StateGraph(State)
+
+workflow.add_node("agent", agent)
+workflow.add_node("tool", tool_node)
+
+workflow.add_edge(START, "agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    "agent",
+    shold_continue,
+    {
+        "continue": "tool",
+        # Otherwise we finish,
+        "end": END
+    }
+)
+
+# We now add a normal edge from 'tools' to 'agent'.
+# This means that after 'tool' is called, 'agent' node is called next.
+workflow.add_edge("tool", "agent")
+
+# Set up memory
+memory = MemorySaver()
+
+# tool node 실행 앞에서 멈추겠다. local 에서 repl 사용의 경우 사용자의 pc에 위협이 되는 코드를 생성 후 실행할 수 있기 때문.
+graph = workflow.compile(checkpointer=memory, interrupt_before=["tools"])
+
+from IPython.display import Image, display
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+import asyncio
+initial_input = {"messages": [HumanMessage(content="미국의 최근 5개년(~2023) GDP 차트를 그려줄래?")]}
+thread = {"configurable": {"thread_id": "13"}}
+async for chunk in graph.astream(initial_input, thread, stream_mode="updates"):
+    for node, values in chunk.items():
+        print(f"Receiving update from node: '{node}'")
+        print(values)
+        print("\n\n")
+
+# 위에서 thread_id를 주고 memory savor에 저장되도록 하였음으로, 위의 결과값을 가진채로 실행하게 된다.
+async def stream_async():
+    async for chunk in graph.astream(None, thread, stream_mode="updates"):
+        for node, values in chunk.items():
+            print(f"Receiving update from node: '{node}'")
+            print(values)
+            print("\n\n")
+asyncio.run(stream_async())
+
+async def stream_async():
+    async for chunk in graph.astream(None, thread, stream_mode="updates"):
+        for node, values in chunk.items():
+            print(f"Receiving update from node: '{node}'")
+            print(values)
+            print("\n\n")
+asyncio.run(stream_async())
+
+async def stream_async():
+    async for chunk in graph.astream(None, thread, stream_mode="updates"):
+        for node, values in chunk.items():
+            print(f"Receiving update from node: '{node}'")
+            print(values)
+            print("\n\n")
+asyncio.run(stream_async())
---
+++ langgraphs/lang_graph/prompt_single_agent.py
@@ -0,0 +1,141 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+
+from dotenv import load_dotenv
+
+# open ai key랑 tavily api key 필요
+load_dotenv()
+
+# 필요 라이브러리 호출
+from typing import List, Annotated, Literal
+from typing_extensions import TypedDict
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, SystemMessage, HumanMessage, ToolMessage
+# inmemory 기억력
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from pydantic import BaseModel
+
+# 프롬프트 작성을 위한 정보 수집 함수 정의하기
+template = """Your job is to get information from a user about what type of prompt template the want to create.
+
+You should get the following information from them:
+
+- What the objective of the prompt is
+- What variables will be passed into the prompt template
+- Any constraints for what the output should NOT do
+- Any requirements that the output MUST adhere to
+
+If you are not able to discern this info, ask them to clarify! Do not attempt to wildy guess.
+
+After you are able to discern all the information, call the relevant tool."""
+
+# 본인의 역할을 잊어버리지 않고, 들고있도록
+def get_messages_info(messages):
+    return [SystemMessage(content = template)] + messages
+
+class PromptInstructions(BaseModel):
+    """Instructions on how to prompt the LLM."""
+
+    objective: str
+    variable: List[str]
+    constraints: List[str]
+    requirements: List[str]
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+# PromptInstructions의 4가지 변수를 output으로 만들어야 한다
+llm_with_tool = llm.bind_tools([PromptInstructions])
+
+def info_chain(state):
+    messages = get_messages_info(state["messages"])
+    response = llm_with_tool.invoke(messages)
+    return {"messages": {response}}
+
+# 프롬프트 작성 함수 정의하기
+from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
+
+# New System Prompt
+# reqs에 들어갈 값은 PromptInstructions class의 4가지 변수
+prompt_system = """Based on the following requirements, write a good prompt template:
+
+{reqs}"""
+
+# Functrion to get the messages for the prompt
+# Will only get messages AFTER the tool call
+def get_prompt_messages(messages: list):
+    tool_call = None
+    other_msgs = []
+    for m in messages:
+        if isinstance(m, AIMessage) and m.tool_calls:
+            tool_call = m.tool_calls[0]["args"]
+            print(tool_call)
+        elif isinstance(m, ToolMessage):
+            continue
+        elif tool_call is not None:
+            other_msgs.append(m)
+    return [SystemMessage(content=prompt_system.format(reqs=tool_call))] + other_msgs
+
+def prompt_gen_chain(state):
+    messages = get_prompt_messages(state["messages"])
+    response = llm.invoke(messages)
+    return {"messages": {response}}
+
+# 프롬프트 구성에 필요한 정보 수집을 완료하기 위한 edge 로직
+from typing import Literal
+from langgraph.graph import END
+
+def get_state(state) -> Literal["prompt", "info", "__end__"]:
+    messages = state["messages"]
+    if isinstance(messages[-1], AIMessage) and messages[-1].tool_calls:
+        return "prompt"
+    elif not isinstance(messages[-1], HumanMessage):
+        return END
+    return "info"
+
+# 그래프 구축하기
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+memory = MemorySaver()
+workflow = StateGraph(State)
+
+# 노드 추가
+workflow.add_node("info", info_chain)
+workflow.add_node("prompt", prompt_gen_chain)
+
+# 엣지 및 조건부 엣지 추가
+workflow.add_conditional_edges("info", get_state)
+workflow.add_edge("prompt", END)
+workflow.add_edge(START, "info")
+
+# 그래프 컴파일
+graph = workflow.compile(checkpointer=memory)
+
+# 그래프 시각화
+from IPython.display import Image, display
+
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+# 그래프 실행
+import uuid
+
+config = {"configurable": {"thread_id": str(uuid.uuid4())}}
+while True:
+    user = input("User (q/Q to quit): ")
+    if user in {"q", "Q"}:
+        print("AI: Byebye")
+        break
+    output = None
+    # update가 된 state만 출력
+    for output in graph.stream(
+        {"messages": [HumanMessage(content=user)]}, config=config, stream_mode="updates"
+    ):
+        # 마지막 메세지 출력
+        last_message = next(iter(output.values()))["messages"][-1]
+        last_message.pretty_print()
+
+    if output and "prompt" in output:
+        print("Done!")
